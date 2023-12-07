caf1e62bbc99eb74016e42660755e7f550ae01f5
Tvgo77 <tvgoshao77@gmail.com>
2023-12-07 06:35:34 +0000

    Server Top Design

--- server/main.go
+++ server/main.go
@@ -7,12 +7,18 @@ import (
 func main() {
 	fmt.Printf("\n")
 	/* Read configuration from file */
+	// env := util.GetEnv()
 
 	/* Init Data channel */
+	// var channelMap = make(map[int]chan int)
 
 	/* Setup SensorManager */
+	// sensorServer := sensorManager.Setup(env, channelMap)
 
 	/* Setup UserManager */
+	// userServer := userManager.Setup(env, channelMap)
 
 	/* Run */
+	// go sensorServer.Run()
+	// go userServer.Run()
 }
