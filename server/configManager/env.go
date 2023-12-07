40f7642a850c6e75585af6f4cd5021c8dd7edc9d
Tvgo77 <tvgoshao77@gmail.com>
2023-12-07 08:57:24 +0000

    Config reading implementation

---
+++ server/configManager/env.go
@@ -0,0 +1,44 @@
+package configManager
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Env struct {
+	AppEnv                 string `mapstructure:"APP_ENV"`
+	ServerPort             int    `mapstructure:"SERVER_PORT"`
+	SensorPort             int    `mapstructure:"SENSOR_PORT"`
+	ContextTimeout         int    `mapstructure:"CONTEXT_TIMEOUT"`
+	DBHost                 string `mapstructure:"DB_HOST"`
+	DBPort                 string `mapstructure:"DB_PORT"`
+	DBUser                 string `mapstructure:"DB_USER"`
+	DBPass                 string `mapstructure:"DB_PASS"`
+	DBName                 string `mapstructure:"DB_NAME"`
+	AccessTokenExpiryHour  int    `mapstructure:"ACCESS_TOKEN_EXPIRY_HOUR"`
+	RefreshTokenExpiryHour int    `mapstructure:"REFRESH_TOKEN_EXPIRY_HOUR"`
+	AccessTokenSecret      string `mapstructure:"ACCESS_TOKEN_SECRET"`
+	RefreshTokenSecret     string `mapstructure:"REFRESH_TOKEN_SECRET"`
+}
+
+func GetEnv() *Env {
+	env := Env{}
+	viper.SetConfigFile(".env")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal("Can't find the file .env : ", err)
+	}
+
+	err = viper.Unmarshal(&env)
+	if err != nil {
+		log.Fatal("Environment can't be loaded: ", err)
+	}
+
+	if env.AppEnv == "development" {
+		log.Println("The App is running in development env")
+	}
+
+	return &env
+}
--- server/main.go
+++ server/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"IoT-backend/server/configManager"
 	"fmt"
 )
+
+func dummyUse(dummy interface{}) {}
 
 func main() {
 	fmt.Printf("\n")
 	/* Read configuration from file */
-	// env := util.GetEnv()
+	env := configManager.GetEnv()
+	dummyUse(env)
 
 	/* Init Data channel */
 	// var channelMap = make(map[int]chan int)
