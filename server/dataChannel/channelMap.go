f45904f6fe8f673143b71427926063ca66f95359
Tvgo77 <tvgoshao77@gmail.com>
2023-12-08 07:21:11 +0000

    mutex channelMap implementation

--- server/dataChannel/channelMap.go
+++ server/dataChannel/channelMap.go
@@ -1,3 +1,35 @@
 package dataChannel
 
-type ChannelMap (map[int]chan int)
+import (
+	"sync"
+)
+
+type ChannelMap struct {
+	mu    sync.Mutex
+	table map[int]chan int
+}
+
+func NewChannelMap() *ChannelMap {
+	return &ChannelMap{
+		table: make(map[int]chan int),
+	}
+}
+
+func (chanMap *ChannelMap) Insert(serialNum int, ch chan int) {
+	chanMap.mu.Lock()
+	chanMap.table[serialNum] = ch
+	chanMap.mu.Unlock()
+}
+
+func (chanMap *ChannelMap) Delete(serialNum int) {
+	chanMap.mu.Lock()
+	delete(chanMap.table, serialNum)
+	chanMap.mu.Unlock()
+}
+
+func (chanMap *ChannelMap) GetChannel(serialNum int) chan int {
+	chanMap.mu.Lock()
+	ch := chanMap.table[serialNum]
+	chanMap.mu.Unlock()
+	return ch
+}
