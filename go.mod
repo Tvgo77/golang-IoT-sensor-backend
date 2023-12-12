87e8877e61e360504c73201d30036711cfd1f46c
Tvgo77 <tvgoshao77@gmail.com>
2023-12-12 13:33:02 +0000

    intergrade mongo interface

--- go.mod
+++ go.mod
@@ -5,6 +5,18 @@ go 1.21.4
 require github.com/spf13/viper v1.18.0
 
 require (
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+)
+
+require (
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
@@ -17,6 +29,7 @@ require (
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	go.mongodb.org/mongo-driver v1.13.1
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
--- server/userManager/user_server.go
+++ server/userManager/user_server.go
@@ -3,6 +3,11 @@ package userManager
 import (
 	"IoT-backend/server/configManager"
 	"IoT-backend/server/dataChannel"
+	"IoT-backend/server/userManager/mongo"
+	"context"
+	"fmt"
+	"log"
+	"time"
 )
 
 type userServer struct {
@@ -16,3 +21,61 @@ func Setup(ev *configManager.Env, chMap *dataChannel.ChannelMap) *userServer {
 		channelMap: chMap,
 	}
 }
+
+func (s *userServer) Run() {
+	/* Database setup */
+	client := NewMongoDatabase(s.env)
+	db := client.Database(s.env.DBName)
+
+	dummy(db)
+
+	/* Router setup */
+}
+
+func NewMongoDatabase(env *configManager.Env) mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	dbHost := env.DBHost
+	dbPort := env.DBPort
+	dbUser := env.DBUser
+	dbPass := env.DBPass
+
+	mongodbURI := fmt.Sprintf("mongodb://%s:%s@%s:%s", dbUser, dbPass, dbHost, dbPort)
+
+	if dbUser == "" || dbPass == "" {
+		mongodbURI = fmt.Sprintf("mongodb://%s:%s", dbHost, dbPort)
+	}
+
+	client, err := mongo.NewClient(mongodbURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client
+}
+
+func CloseMongoDBConnection(client mongo.Client) {
+	if client == nil {
+		return
+	}
+
+	err := client.Disconnect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connection to MongoDB closed.")
+}
+
+func dummy(any interface{}) {}
