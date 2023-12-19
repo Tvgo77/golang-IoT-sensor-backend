e798641912ed2cb1f7793fe636831039c448bfe0
Tvgo77 <tvgoshao77@gmail.com>
2023-12-19 08:17:57 +0000

    Add refresh token API

---
+++ server/userManager/api/controller/refresh_token_controller.go
@@ -0,0 +1,61 @@
+package controller
+
+import (
+	"net/http"
+
+	"IoT-backend/server/configManager"
+	"IoT-backend/server/userManager/domain"
+
+	"github.com/gin-gonic/gin"
+)
+
+type RefreshTokenController struct {
+	RefreshTokenUsecase domain.RefreshTokenUsecase
+	Env                 *configManager.Env
+}
+
+func (rtc *RefreshTokenController) RefreshToken(c *gin.Context) {
+	var request domain.RefreshTokenRequest
+
+	// Check request format
+	err := c.ShouldBind(&request)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, domain.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	// Parse Token to get user ID
+	id, err := rtc.RefreshTokenUsecase.ExtractIDFromToken(request.RefreshToken, rtc.Env.RefreshTokenSecret)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: "User not found"})
+		return
+	}
+
+	// Check if user exist
+	user, err := rtc.RefreshTokenUsecase.GetUserByID(c, id)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: "User not found"})
+		return
+	}
+
+	// Generate new access token and refresh token
+	accessToken, err := rtc.RefreshTokenUsecase.CreateAccessToken(&user, rtc.Env.AccessTokenSecret, rtc.Env.AccessTokenExpiryHour)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	refreshToken, err := rtc.RefreshTokenUsecase.CreateRefreshToken(&user, rtc.Env.RefreshTokenSecret, rtc.Env.RefreshTokenExpiryHour)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	// HTTP response
+	refreshTokenResponse := domain.RefreshTokenResponse{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}
+
+	c.JSON(http.StatusOK, refreshTokenResponse)
+}
