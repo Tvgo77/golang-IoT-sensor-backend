ce7e22715ffb37d32a8c1f01859f9ad5e19a1a57
Tvgo77 <tvgoshao77@gmail.com>
2023-12-19 03:59:49 +0000

    signup router complete

--- server/userManager/api/route/route.go
+++ server/userManager/api/route/route.go
@@ -2,8 +2,10 @@ package route
 
 import (
 	"IoT-backend/server/configManager"
+	"IoT-backend/server/userManager/api/controller"
 	"IoT-backend/server/userManager/mongo"
 	"IoT-backend/server/userManager/repository"
+	"IoT-backend/server/userManager/usecase"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -13,13 +15,13 @@ func dummy(any interface{}) {}
 
 func Setup(env *configManager.Env, timeout time.Duration, db mongo.Database,
 	gin *gin.Engine) {
+	// Public APIs
 	publicRouter := gin.Group("")
-	dummy(publicRouter)
-
+	NewSignupRouter(env, timeout, db, publicRouter)
 }
 
 func NewSignupRouter(env *configManager.Env, timeout time.Duration, db mongo.Database, group *gin.RouterGroup) {
-	ur := repository.NewUserRepository(db, domain.CollectionUser)
+	ur := repository.NewUserRepository(db, "Users")
 	sc := controller.SignupController{
 		SignupUsecase: usecase.NewSignupUsecase(ur, timeout),
 		Env:           env,
